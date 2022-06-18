5646dd5b3478fff776d439ff8e863e49a144553d
nisabmohd <nisabmohd@gmail.com>
2022-06-18 09:50:21 +0530

    Added few more methods in LinkedMap

--- src/Maps/LinkedHashMapCustom.java
+++ src/Maps/LinkedHashMapCustom.java
@@ -1,23 +1,21 @@
 package Maps;
 
-import java.util.*;
-
 public class LinkedHashMapCustom<K, V> {
 
     private final int DEFAULT_CAPACITY = 17;
-    private Node previous = null;
-    private Node[] bucket = new Node[DEFAULT_CAPACITY];
-    private Node root = null;
+    private Entry previous = null;
+    private final Entry[] bucket = new Entry[DEFAULT_CAPACITY];
+    private Entry root = null;
 
-    private class Node<K, V> {
+    private class Entry<K, V> {
 
         K key;
         V val;
-        Node next;
-        Node after;
-        Node before;
+        Entry next;
+        Entry after;
+        Entry before;
 
-        public Node(K key, V val, Node next, Node after, Node before) {
+        public Entry(K key, V val, Entry next, Entry after, Entry before) {
             this.key = key;
             this.val = val;
             this.next = next;
@@ -34,16 +32,16 @@ public class LinkedHashMapCustom<K, V> {
     public void put(K key, V val) {
 
         int hash = hashCode(key);
-        Node t = new Node(key, val, null, null, previous);
+        Entry t = new Entry(key, val, null, null, previous);
         if (bucket[hash] == null) {
             bucket[hash] = t;
         } else {
-            Node temp = bucket[hash];
+            Entry temp = bucket[hash];
             while (temp.next != null) {
                 if (temp.key == key) {
                     temp.val = val;
                     return;
-                };
+                }
                 temp = temp.next;
             }
             temp.next = t;
@@ -58,24 +56,12 @@ public class LinkedHashMapCustom<K, V> {
         }
     }
 
-    @Override
-    public String toString() {
-        String ret = "[ ";
-        Node temp = root;
-        while (temp != null) {
-            ret += temp.key + "=" + temp.val + " ";
-            temp = temp.after;
-        }
-        ret += "]";
-        return ret;
-    }
-
     public V get(K key) {
         int hash = hashCode(key);
         if (bucket[hash] == null) {
             return null;
         }
-        Node temp = bucket[hash];
+        Entry temp = bucket[hash];
         while (temp != null) {
             if (temp.key == key) {
                 return (V) temp.val;
@@ -87,14 +73,11 @@ public class LinkedHashMapCustom<K, V> {
     }
 
     public boolean containsKey(K key) {
-        if (get(key) == null) {
-            return false;
-        }
-        return true;
+        return get(key) != null;
     }
 
     public boolean containsValue(V val) {
-        Node temp = root;
+        Entry temp = root;
         while (temp != null) {
             if (temp.val == val) {
                 return true;
@@ -105,8 +88,74 @@ public class LinkedHashMapCustom<K, V> {
 
     }
 
-}
+    public void remove(K key) throws Exception {
+        int hash = hashCode(key);
+        Entry temp = bucket[hash];
+        if (temp.key == key) {
+            if (temp == root) {
+                root = temp.after;
+            }
+            if (temp.before != null) {
+                temp.before.after = temp.after;
+            }
+            if (temp.after != null) {
+                temp.after.before = temp.before;
+            }
+            bucket[hash] = temp.next;
+        } else {
+            while (temp.next != null) {
+                if (temp.next.key == key) {
+                    break;
+                }
+                temp = temp.next;
+            }
+            if (temp != null) {
+                Entry delete = temp.next;
+                if (delete == null) {
+                    // you can remove exception if you dont want and replace it it return
+                    throw new Exception("No such element exception");
+                    //return;
+                }
+                if (delete.before != null) {
+                    delete.before.after = delete.after != null ? delete.after : null;
+                }
+                if (delete.after != null) {
+                    delete.after.before = delete.before != null ? delete.before : null;
+                }
+                temp.next = temp.next.next;
+            }
+        }
+    }
 
+    public V getOrDefault(K key, V defaultValue) {
+        V value = get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public void clear() {
+        root = null;
+        previous = null;
+        for (int i = 0; i < bucket.length; i++) {
+            bucket[i] = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String ret = "[ ";
+        Entry temp = root;
+        while (temp != null) {
+            ret += temp.key + "=" + temp.val + " ";
+            temp = temp.after;
+        }
+        ret += "]";
+        return ret;
+    }
+
+}
 
 // check out the structure of LinkedHashMap image to understand more better
 //https://lh4.googleusercontent.com/5M89iub9wNrKR-jmctcmLtGIHiy0lbqL5Oz18AvQyuHdFXGDC4qKgCVZO73WPujj3qqquh_WXlKp7zem4vCfEynJzcN-t0a_wa_mSxDO62G2C-YgEvUhQRBdXdWvpBIhDDM0BxE
--- src/Maps/Main.java
+++ src/Maps/Main.java
@@ -2,7 +2,7 @@ package Maps;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        TreeMapCustom<String, Integer> map = new TreeMapCustom<>();
 //        map.put("t", 70);
 //        map.put("b", 150);
@@ -10,11 +10,20 @@ public class Main {
 //        map.put("d", 150);
 //        map.display();
         LinkedHashMapCustom<Integer, Integer> map = new LinkedHashMapCustom<>();
-        map.put(58, 78); map.put(8, 8); 
-        map.put(99, 48); map.put(58, 58); map.put(18, 178);
+        map.put(58, 78);
+        map.put(8, 8);
+        map.put(99, 48);
+        map.put(58, 58);
+        map.put(18, 178);
+        map.remove(18);
+        map.remove(58);
+        map.remove(99);
+//        map.remove(8);
 //        System.out.println(map.containsKey(68));
-        System.out.println(map.containsValue(58));
+//        System.out.println(map.containsValue(58));
+//        map.clear();
+//        System.out.println(map.getOrDefault(99, 46));
+        System.out.println(map);
 
-        
     }
 }
