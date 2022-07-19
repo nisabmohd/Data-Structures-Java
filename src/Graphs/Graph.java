1cc562f488f391eff0e3f1830bac0f568141ff90
nisabmohd <nisabmohd@gmail.com>
2022-07-20 01:54:48 +0530

    Added Graph

---
+++ src/Graphs/Graph.java
@@ -0,0 +1,37 @@
+package Graphs;
+
+import java.util.*;
+
+public class Graph {
+
+    private ArrayList<LinkedList<Integer>> list;
+
+    public Graph(int size) {
+        list = new ArrayList<>();
+        for (int i = 0; i <= size; i++) {
+            list.add(new LinkedList<>());
+        }
+    }
+
+    public void addEdge(int source, int dest) {
+        list.get(source).add(dest);
+        list.get(dest).add(source);
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < list.size(); i++) {
+            builder.append(i);
+            builder.append(" = ");
+            int j = 0;
+            for (; j < list.get(i).size() - 1; j++) {
+                builder.append(list.get(i).get(j));
+                builder.append("->");
+            }
+            builder.append(list.get(i).get(j));
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+}
